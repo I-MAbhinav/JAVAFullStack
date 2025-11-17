3cff5afeccb329b5b4d1b9565e0ac398544db09f
I-MAbhinav <shakyaabhinav224@gmail.com>
2025-11-17 14:52:43 +0530

    Changed

---
+++ Day11/BackendDev/model/Todo.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new  mongoose.Schema({
+   title:{
+    type:String,
+    required:true,
+    maxLength:64
+   },
+   description:{
+    type:String,
+    required:true
+   },
+   createdAt:{
+    type:Date,
+    default:Date.now(),
+   },
+    updatedAt:{
+    type:Date,
+    default:Date.now(),
+   }
+})
+
+module.exports = mongoose.model("Todo", todoSchema);
